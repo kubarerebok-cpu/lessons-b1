8b5c94c68eb2ed331f8660a98601e4b9c92782f1
Your Name <you@example.com>
2026-09-18 16:05:30 +0600

    first commit

---
+++ lesson1/index.js
@@ -0,0 +1,12 @@
+// alert("hello world");
+// confirm("hello world");
+// prompt("what is your name");
+
+// console.log("hello world");
+
+// let name = "kuba"
+// alert(name);
+
+// let name = prompt("what is your name?");
+// alert(name)
+
